eb83a6be04d1426302d2733a9c6965126c5f7986
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-18 07:08:23 +1000

    Create yazi.nix

---
+++ modules/profiles/desktop/hyprland/yazi.nix
@@ -0,0 +1,14 @@
+{
+
+  flake.modules.homeManager.yazi = {
+
+    programs.yazi = {
+      enable = true;
+      settings = {
+        mgr = {
+         show_hidden = true;
+        };
+      };
+    enableFishIntegration = true;
+  };
+}

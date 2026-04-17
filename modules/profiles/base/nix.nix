8caff4627dd8e9fc18e707cffb0fcf7933966c8c
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-17 20:31:49 +1000

    Create nix.nix

---
+++ modules/profiles/base/nix.nix
@@ -0,0 +1,20 @@
+{
+  flake.modules.nixos.base = {
+
+    nixpkgs.config.allowUnfree = true;
+
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    programs.nh = {
+      enable = true;
+      flake = "/home/sam/.dotfiles";
+      clean = {
+        enable = true;
+        extraArgs = "--keep-since 14d";
+      };
+    };
+  };
+}

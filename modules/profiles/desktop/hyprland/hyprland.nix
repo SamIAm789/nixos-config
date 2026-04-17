7b51ba15925262aba09e9b826d2e680cedf5b05f
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-17 22:26:30 +1000

    Create hyprland.nix

---
+++ modules/profiles/desktop/hyprland/hyprland.nix
@@ -0,0 +1,74 @@
+{
+  flake.modules.nixos.hyprland = {
+
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    };
+
+    # services.displayManager.gdm.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      kitty
+      swaynotificationcenter
+      #hyprpolkitagent
+      nautilus
+      bolt
+      bluetuith
+
+    ];
+
+    #programs.ssh.startAgent = true;
+
+    programs.kdeconnect.enable = true;
+
+    #screensharing
+    #xdg.portal = {
+    #  enable = true;
+    #  extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+    #};
+
+    services = {
+      pipewire.enable = true;
+      upower.enable = true;
+      hardware.bolt.enable = true;
+      tlp.enable = true;
+    };
+
+    # general desktop optimisations
+    services.dbus.implementation = "broker";
+    # networking.networkmanager.wifi.backend = "iwd";
+
+    hardware.bluetooth.enable = true;
+
+    services.flatpak.enable = true;
+
+    # needed for hyprlock
+    security.pam.services.hyprlock = {};
+
+    fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
+
+    #polkit
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.hyprland.enableGnomeKeyring = true;
+    security.polkit.enable=true;
+
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+
+    home-manager.users.sam = {
+      imports = [
+        ../../home/hyprland/hyprland.nix
+      ];
+    };
+
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+     
+  }

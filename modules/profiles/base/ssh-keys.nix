5b403b1b403fbfed3046272b7c960dec64aeffef
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-22 15:21:54 +1000

    Create ssh-keys.nix

---
+++ modules/profiles/base/ssh-keys.nix
@@ -0,0 +1,9 @@
+{
+  flake.modules.nixos.ssh-keys = {
+
+    users.users.sam.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHptkyokSn8XreYaJQUyy1UPF8qiAq2cjCat3zPMO5Z user" #framework
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHV+lxnSONakDVuDrD3qcKWLKj4JCu05VEe8EeyCDIB u0_a402@localhost" #termux
+    ];
+  };
+}

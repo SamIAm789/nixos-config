786416ae9245577280d45302aa145098ef888777
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-22 14:20:29 +1000

    Update nebula-2.nix

--- modules/factory/nebula-2.nix
+++ modules/factory/nebula-2.nix
@@ -91,3 +91,107 @@
       environment.systemPackages = [ pkgs.nebula ];
     };
 }
+
+
+
+{ lib, ... }:
+{
+  config.flake.factory.nebula-host =
+    { isLighthouse ? false }:
+
+    { config, pkgs, ... }:
+
+    let
+      host = config.networking.hostName;
+
+      networkName = "pertaka";
+      listenPort = 4242;
+
+      sopsFile = /secrets/nebula.yaml;
+
+      nebulaUser = "nebula-pertaka";
+      nebulaGroup = "nebula-pertaka";
+
+      # Helper for host-specific secrets
+      secret = name:
+        config.sops.secrets."nebula/${name}".path;
+
+      # Lighthouse IP as literal string
+      lighthouseIP =
+        builtins.readFile
+          config.sops.secrets."nebula/lighthouse_ip".path;
+
+    in
+    {
+      # --- SOPS secrets ---
+      sops.secrets = {
+        "nebula/ca" = {
+          inherit sopsFile;
+          owner = nebulaUser;
+          group = nebulaGroup;
+          mode = "0400";
+        };
+
+        "nebula/${host}/cert" = {
+          inherit sopsFile;
+          owner = nebulaUser;
+          group = nebulaGroup;
+          mode = "0400";
+        };
+
+        "nebula/${host}/key" = {
+          inherit sopsFile;
+          owner = nebulaUser;
+          group = nebulaGroup;
+          mode = "0400";
+        };
+
+        "nebula/lighthouse_ip" = {
+          inherit sopsFile;
+          owner = nebulaUser;
+          group = nebulaGroup;
+          mode = "0400";
+        };
+      };
+
+      # --- Nebula service ---
+      services.nebula.networks.${networkName} = {
+        ca   = secret "ca";
+        cert = secret "${host}/cert";
+        key  = secret "${host}/key";
+
+        staticHostMap = {
+          "100.100.0.1" = [ lighthouseIP ];
+        };
+
+        lighthouses =
+          lib.mkIf (!isLighthouse) [ "100.100.0.1" ];
+
+        settings = {
+          lighthouse = {
+            am_lighthouse = isLighthouse;
+            interval = 60;
+          };
+
+          punchy = {
+            punch = true;
+            respond = true;
+          };
+        };
+
+        firewall = {
+          outbound = [
+            { host = "any"; port = "any"; proto = "any"; }
+          ];
+          inbound = [
+            { host = "any"; port = "any"; proto = "any"; }
+          ];
+        };
+
+        relays =
+          lib.mkIf (!isLighthouse) [ "100.100.0.1" ];
+      };
+
+      environment.systemPackages = [ pkgs.nebula ];
+    };
+}

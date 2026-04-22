4dfdf3747d97ea8f7a9af91f8d6019de015e70ec
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-22 13:55:54 +1000

    Update nebula.nix

--- modules/factory/nebula.nix
+++ modules/factory/nebula.nix
@@ -9,70 +9,107 @@
     { config, pkgs, ... }:
 
     let
-      # Hostname comes from system config
-      hostName = config.networking.hostName;
+      # Hostname from system config
+      host = config.networking.hostName;
 
       # Hardcoded network + port
       networkName = "pertaka";
       listenPort = 4242;
 
-      # Shared SOPS file from private repo
+      # SOPS file from private repo
       sopsFile = /secrets/nebula.yaml;
 
-      # Helper for host-specific secrets
-      secret = name:
-        config.sops.secrets."${secretsPrefix}/${hostName}/${name}".path;
+      # Nebula user/group
+      nebulaUser = "nebula-pertaka";
+      nebulaGroup = "nebula-pertaka";
 
-      # Lighthouse IP must be read as a literal string
+      # List of secrets exactly like your working module
+      nebulaSecrets = [
+        "ca_crt"
+        "backup-server_crt"
+        "backup-server_key"
+        "framework_crt"
+        "framework_key"
+        "haos_crt"
+        "haos_key"
+        "server_crt"
+        "server_key"
+      ];
+
+      # Helper to get a secret path
+      secretPath = name:
+        config.sops.secrets."nebula_${name}".path;
+
+      # Lighthouse IP stored in SOPS and read as a literal string
       lighthouseIP =
         builtins.readFile
-          config.sops.secrets."${secretsPrefix}/lighthouse/ip".path;
+          config.sops.secrets."nebula_lighthouse_ip".path;
 
     in
     {
-      # --- SOPS secrets ---
-      sops.secrets = {
-        # Host certs
-        "${secretsPrefix}/${hostName}/ca"      = { inherit sopsFile; };
-        "${secretsPrefix}/${hostName}/cert"    = { inherit sopsFile; };
-        "${secretsPrefix}/${hostName}/key"     = { inherit sopsFile; };
-        "${secretsPrefix}/${hostName}/hostmap" = { inherit sopsFile; };
-
-        # Shared lighthouse IP secret
-        "${secretsPrefix}/lighthouse/ip" = {
-          inherit sopsFile;
-          owner = "root";
-          mode = "0400";
+      # --- SOPS secrets (same as your module, but generated) ---
+      sops.secrets =
+        builtins.listToAttrs (map (name: {
+          name = "nebula_${name}";
+          value = {
+            inherit sopsFile;
+            owner = nebulaUser;
+            group = nebulaGroup;
+            mode = "0400";
+          };
+        }) nebulaSecrets)
+        //
+        {
+          # Lighthouse IP secret
+          nebula_lighthouse_ip = {
+            inherit sopsFile;
+            owner = nebulaUser;
+            group = nebulaGroup;
+            mode = "0400";
+          };
         };
-      };
 
       # --- Nebula service ---
-      services.nebula = {
-        enable = true;
+      services.nebula.networks.${networkName} = {
+        ca   = secretPath "ca_crt";
+        cert = secretPath "${host}_crt";
+        key  = secretPath "${host}_key";
 
-        networks.${networkName} = {
-          enable = true;
+        # Static host map always includes lighthouse
+        staticHostMap = {
+          "100.100.0.1" = [ lighthouseIP ];
+        };
 
-          ca   = secret "ca";
-          cert = secret "cert";
-          key  = secret "key";
+        # Non-lighthouse nodes point to lighthouse
+        lighthouses =
+          lib.mkIf (!isLighthouse) [ "100.100.0.1" ];
 
-          staticHostMapFile = secret "hostmap";
-
-          listen = {
-            host = "0.0.0.0";
-            port = listenPort;
+        settings = {
+          lighthouse = {
+            am_lighthouse = isLighthouse;
+            interval = 60;
           };
 
-          lighthouse = {
-            amLighthouse = isLighthouse;
-            interval = 60;
-
-            hosts = lib.optionals (!isLighthouse) [
-              lighthouseIP
-            ];
+          punchy = {
+            punch = true;
+            respond = true;
           };
         };
+
+        firewall = {
+          outbound = [
+            { host = "any"; port = "any"; proto = "any"; }
+          ];
+          inbound = [
+            { host = "any"; port = "any"; proto = "any"; }
+          ];
+        };
+
+        relays =
+          lib.mkIf (!isLighthouse) [ "100.100.0.1" ];
       };
+
+      # Nebula binary
+      environment.systemPackages = [ pkgs.nebula ];
     };
 }

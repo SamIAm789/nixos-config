da208c3df40d34df3f6000f6a0cc4a3907e66200
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-22 13:23:47 +1000

    Update nebula.nix

--- modules/factory/nebula.nix
+++ modules/factory/nebula.nix
@@ -1,18 +1,72 @@
+# nix/aspects/nebula.nix
+{ lib, ... }:
 {
-  config.flake.factory.user = username: isAdmin: {
+  config.flake.factory.nebula-host =
+    { hostName
+    , isLighthouse ? false
+    , secretsPrefix ? "nebula"
+    , sopsFile
+    }:
 
-    darwin."${username}" = {
-      users.users."${username}" = {
-        name = "${username}";
-      };
-      system.primaryUser = lib.mkIf isAdmin "${username}";
-    };
+    { config, pkgs, ... }:
 
-    nixos."${username}" = {
-      users.users."${username}" = {
-        name = "${username}";
+    let
+      networkName = "pertaka";
+      listenPort = 4242;
+
+      # host-specific secret path helper
+      secret = name:
+        config.sops.secrets."${secretsPrefix}/${hostName}/${name}".path;
+
+      # lighthouse IP must be read as a string
+      lighthouseIP =
+        builtins.readFile
+          config.sops.secrets."${secretsPrefix}/lighthouse/ip".path;
+
+    in
+    {
+      # --- SOPS secrets ---
+      sops.secrets = {
+        "${secretsPrefix}/${hostName}/ca"      = { inherit sopsFile; };
+        "${secretsPrefix}/${hostName}/cert"    = { inherit sopsFile; };
+        "${secretsPrefix}/${hostName}/key"     = { inherit sopsFile; };
+        "${secretsPrefix}/${hostName}/hostmap" = { inherit sopsFile; };
+
+        # shared lighthouse IP secret
+        "${secretsPrefix}/lighthouse/ip" = {
+          inherit sopsFile;
+          owner = "root";
+          mode = "0400";
+        };
       };
-      extraGroups = lib.optionals isAdmin [ "wheel" ];
+
+      # --- Nebula service ---
+      services.nebula = {
+        enable = true;
+
+        networks.${networkName} = {
+          enable = true;
+
+          ca   = secret "ca";
+          cert = secret "cert";
+          key  = secret "key";
+
+          staticHostMapFile = secret "hostmap";
+
+          listen = {
+            host = "0.0.0.0";
+            port = listenPort;
+          };
+
+          lighthouse = {
+            amLighthouse = isLighthouse;
+            interval = 60;
+
+            hosts = lib.optionals (!isLighthouse) [
+              lighthouseIP
+            ];
+          };
+        };
+      };
     };
-  };
 }

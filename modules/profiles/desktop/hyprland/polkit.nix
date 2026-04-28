f04a33a35a8ad02ca48f8f83e42589b6e2548f19
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-28 17:42:49 +1000

    Update polkit.nix

--- modules/profiles/desktop/hyprland/polkit.nix
+++ modules/profiles/desktop/hyprland/polkit.nix
@@ -4,6 +4,7 @@
     environment.systemPackages = [
       pkgs.hyprpolkitagent
     ];
+    # autostart with systemctl --user enable --now hyprpolkitagent.service
 
     security.polkit.enable = true;
   };

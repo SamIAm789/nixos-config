e3a6067323f2f9bfada4f0aed2776a3ed509923b
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-04-28 17:41:50 +1000

    Update polkit.nix

--- modules/profiles/desktop/hyprland/polkit.nix
+++ modules/profiles/desktop/hyprland/polkit.nix
@@ -1,8 +1,10 @@
 {
   flake.modules.nixos.polkit = {
 
-    services.gnome.gnome-keyring.enable = true;
-    security.pam.services.hyprland.enableGnomeKeyring = true;
+    environment.systemPackages = [
+      pkgs.hyprpolkitagent
+    ];
+
     security.polkit.enable = true;
   };
 }

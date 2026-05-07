5619be13ad520e46b48779943104e9bfde073d45
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-07 11:01:38 +1000

    Implement systemd service for ZFS rollback in initrd
    Add configuration for systemd service in initrd to reset root filesystem using ZFS rollback.

---
+++ modules/tools/impermanence/rollback.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules.nixos.impermanence= {
+
+# Enable systemd in initrd
+  boot.initrd.systemd.enable = true;
+
+  # systemd in initrd requires a service instead of a command
+  boot.initrd.systemd.services.reset = {
+    description = "reset root filesystem";
+    wantedBy = [ "initrd.target" ];
+    after = [ "zfs-import-system.service" ];
+    before = [ "sysroot.mount" ];
+    path = with pkgs; [ zfs ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = "zfs rollback -r server/local/root@blank";
+  };
+}

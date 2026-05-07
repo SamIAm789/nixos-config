04690d104ef8950bc673e97f07c850880aafefdc
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-07 11:20:04 +1000

    Create filesystems.nix

---
+++ modules/hosts/server/filesystems.nix
@@ -0,0 +1,21 @@
+fileSystems."/" = {
+      device = "server/local/root";
+      fsType = "zfs";
+    };
+  
+  fileSystems."/nix" = {
+      device = "server/local/nix";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" = {
+      device = "server/local/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/persist" = {
+      device = "server/local/persist";
+      fsType = "zfs";
+      neededForBoot = true;
+    };
+}

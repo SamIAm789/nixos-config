6860708e61e6625fb9297d17a63524f9bec0ff7b
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-07 21:35:04 +1000

    Update zfs.nix

--- modules/tools/zfs.nix
+++ modules/tools/zfs.nix
@@ -1,9 +1,21 @@
 {
-  flake.modules.nixos.zfs = {
+  flake.modules.nixos.zfs = 
 
-    boot.supportedFilesystems = [ "zfs" ];
-    boot.zfs.forceImportRoot = false;
-    networking.hostId = "6c0ee112";
-    services.zfs.autoScrub.enable = true;
+  let
+    hash = builtins.hashString "sha256" config.networking.hostName;
 
+    hostId = builtins.substring 0 8 hash;
+  in
+  {
+
+    boot = {
+      supportedFilesystems = [ "zfs" ];
+      zfs.forceImportRoot = false;
+    };
+    networking.hostId = hostId;
+    services.zfs = {
+      autoScrub.enable = true;
+      trim.enable = true;
+    };
+  };
 }

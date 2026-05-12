4c183c32dd5b4b3c637fac328020f5f57e0b18d7
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-12 22:36:02 +1000

    Add microvm-host configuration with networking
    Configure microvm-host with networking settings and systemd network management.

--- modules/profiles/server/microvm/microvm-host.nix
+++ modules/profiles/server/microvm/microvm-host.nix
@@ -1,1 +1,43 @@
+{
+  flake.modules.nixos.microvm-host.nix = {
+    microvm.host.enable = true;
 
+    networking = {
+      networkmanager.enable = lib.mkForce false;
+      useDHCP = false;
+    };
+
+    systemd.network = {
+      enable = true;
+
+      netdevs = {
+
+        "microbr" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "microbr";
+          };
+        };
+      };
+
+      networks = {
+
+        "10-lan" = {
+          matchConfig.Name = [ "eno1" "vm-*" ];
+          networkConfig.Bridge = "microbr";
+          linkConfig.RequiredForOnline = "enslaved";
+        };
+
+        "10-lan-microbr" = {
+          matchConfig.Name = "microbr";
+          linkConfig = {
+            RequiredForOnline = "routable";
+          };
+          networkConfig = {
+            DHCP = "yes";
+          };
+        };
+      };
+    };
+  };
+}

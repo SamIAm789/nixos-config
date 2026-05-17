e7b4f3ad61c1c94e7da9f17c70f491f8a2a10a0a
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-18 08:22:24 +1000

    Create server.nix

---
+++ modules/profiles/server/server.nix
@@ -0,0 +1,15 @@
+{
+  inputs,
+  ...
+}:
+{
+  flake.modules.nixos.server-profile = {
+    imports = with inputs.self.modules.nixos; [
+      base
+      microvm-host
+      network
+      openssh
+      sanoid
+    ];
+  };
+}

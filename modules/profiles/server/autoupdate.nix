11febeeff61068ef418ef08064ba8dd448068103
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-18 08:40:07 +1000

    Add autoupdate configuration for NixOS

---
+++ modules/profiles/server/autoupdate.nix
@@ -0,0 +1,34 @@
+{
+  flake.modules.nixos.autoupdate =
+  {
+    pkgs,
+    ...
+  }:
+  {
+    systemd.services.pull-updates = {
+      description = "Pulls changes to system config";
+      restartIfChanged = false;
+      startAt = "01:00";
+      path = [ pkgs.git pkgs.openssh ];
+      script = ''
+        test "$(git branch --show-current)" = "main"
+        git pull --ff-only
+      '';
+      serviceConfig = {
+        WorkingDirectory = "/home/sam/.dotfiles";
+        User = "sam";
+        Type = "oneshot";
+      };
+    };
+
+    system.autoUpgrade = {
+      enable = true;
+      flake = "path:/home/sam/.dotfiles";
+      allowReboot = true;
+      rebootWindow = {
+        lower = "02:00";
+        upper = "04:00";
+      };
+    };
+  };
+}

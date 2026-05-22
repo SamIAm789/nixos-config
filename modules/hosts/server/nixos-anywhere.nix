c820644a236de6150a043a0c386b1bc2a0ed813e
sam <samblackabc@gmail.com>
2026-05-22 10:39:19 +1000

    flake-parts server

--- modules/hosts/server/nixos-anywhere.nix
+++ modules/hosts/server/nixos-anywhere.nix
@@ -7,7 +7,18 @@
   {
     sops.age.keyFile = "/var/lib/sops-nix/key.txt";
     environment.etc."var/lib/sops-nix/key.txt" = {
-      source = "/extra-files/keys.txt";
+      source = "/extra-files/key.txt";
     };
+
+    systemd.tmpfiles.rules = [
+      "d /var/lib/sops-nix 0700 root root -"
+      "f /var/lib/sops-nix/key.txt 0400 root root -"
+    ];
+
+    programs.ssh.extraConfig = ''
+      Host github.com
+        IdentityFile /root/.ssh/id_ed25519
+        IdentitiesOnly yes
+    '';
   };
 }

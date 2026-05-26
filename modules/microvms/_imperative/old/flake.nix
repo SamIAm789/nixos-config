f0404574835a1d45912572a2140bf330b689abc9
sam <samblackabc@gmail.com>
2026-05-26 13:40:02 +1000

    imperative microvm flake

--- modules/microvms/_imperative/old/flake.nix
+++ modules/microvms/_imperative/old/flake.nix
@@ -18,19 +18,22 @@
 
     let
 
-    mkSystem = entrypoint: nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        entrypoint
-        microvm.nixosModules.microvm
-      ];
-    };
+      mkSystem = entrypoint: nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          entrypoint
+          microvm.nixosModules.microvm
+        ];
+      };
 
     in
 
     {
-    immich = mkSystem ./immich.nix;
+      nixosConfigurations.immich = mkMicroVM ./immich.nix;
+
+        # Best for the microvm CLI
+      microvm.config.immich = mkMicroVM ./immich.nix;
     };
   };
 }
--- modules/microvms/_imperative/old/immich.nix
+++ modules/microvms/_imperative/old/immich.nix
@@ -1,29 +1,66 @@
-{ config, lib, pkgs, microvm, inputs, ... }: {
+{ config, lib, pkgs, ... }:
 
+{
   networking.hostName = "immich-vm";
+  system.stateVersion = "25.11";
 
+  # ── MicroVM Configuration ─────────────────────────────────────────────
   microvm = {
-    interfaces = [ {
-        type = "tap";
-        id = "vm-immich";
-        mac = "02:00:00:00:00:01";
-    } ];
-    shares = [ {
+    hypervisor = "cloud-hypervisor";
+    vcpu = 2;
+    mem = 2048;
+    hotplugMem = 4096;
+
+    interfaces = [{
+      type = "tap";
+      id = "vm-immich";
+      mac = "02:00:00:00:00:01";
+    }];
+
+    # Persistent root disk (Very Important!)
+    volumes = [{
+      image = "/persist/microvms/immich/root.img";
+      mountPoint = "/";
+      size = 16384;           # 16GB
+      fsType = "ext4";
+      autoCreate = true;
+    }];
+
+    shares = [
+      {
         proto = "virtiofs";
         tag = "photos";
         source = "/stuff/photos";
         mountPoint = "/stuff/photos";
         socket = "photos.socket";
-    } ];
-
-    vsock.cid = 100;
-    # set dataset options to -o xattr=sa -o acltype=posixacl
-    mem = 2 * 1024;
-    hotplugMem = 4 * 1024; #max amount of extra mem that can be added with virtio-men
-    hypervisor = "cloud-hypervisor";
-    vcpu = 2;
+      }
+      {
+        proto = "virtiofs";
+        tag = "ro-store";
+        source = "/nix/store";
+        mountPoint = "/nix/.ro-store";
+      }
+    ];
   };
 
+  # ── Networking ────────────────────────────────────────────────────────
+  systemd.network.enable = true;
+  systemd.network.networks."20-lan" = {
+    matchConfig.Type = "ether";
+    networkConfig.DHCP = "yes";
+  };
+
+  # ── User ──────────────────────────────────────────────────────────────
+  users.users.sam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "test";   # Change this soon!
+  };
+
+  # ── SSH ───────────────────────────────────────────────────────────────
+  services.openssh.enable = true;
+
+  # ── Immich ────────────────────────────────────────────────────────────
   services.immich = {
     enable = true;
     host = "0.0.0.0";
@@ -32,23 +69,10 @@
     openFirewall = true;
   };
 
-  users.users.sam = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    initialPassword = "test";
-  };
+  # ── Quality of Life ───────────────────────────────────────────────────
+  environment.systemPackages = with pkgs; [
+    htop iotop
+  ];
 
-
-  services.openssh.enable = true;
-
-  systemd.network.enable = true;
-
-  systemd.network.networks."20-lan" = {
-    matchConfig.Type = "ether";
-    networkConfig = {
-      DHCP = "yes";
-    };
-  };
-
-  system.stateVersion = "25.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }

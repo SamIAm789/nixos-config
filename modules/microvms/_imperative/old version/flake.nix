faef2a0f46f151dc9c2c2df7bf4a26793d06f56f
sam <samblackabc@gmail.com>
2026-05-26 13:09:48 +1000

    imperative microvm flake

---
+++ modules/microvms/_imperative/old version/flake.nix
@@ -0,0 +1,36 @@
+{
+
+  description = "Sam's Nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    microvm = {
+      url = "github:microvm-nix/microvm.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, microvm, ... } @inputs:
+
+  {
+
+    nixosConfigurations =
+
+    let
+
+    mkSystem = entrypoint: nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        entrypoint
+        microvm.nixosModules.microvm
+      ];
+    };
+
+    in
+
+    {
+    immich = mkSystem ./immich.nix;
+    };
+  };
+}
---
+++ modules/microvms/_imperative/old version/immich.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, microvm, inputs, ... }: {
+
+  networking.hostName = "immich-vm";
+
+  microvm = {
+    interfaces = [ {
+        type = "tap";
+        id = "vm-immich";
+        mac = "02:00:00:00:00:01";
+    } ];
+    shares = [ {
+        proto = "virtiofs";
+        tag = "photos";
+        source = "/stuff/photos";
+        mountPoint = "/stuff/photos";
+        socket = "photos.socket";
+    } ];
+    # set dataset options to -o xattr=sa -o acltype=posixacl
+    mem = 2 * 1024;
+    hotplugMem = 4 * 1024; #max amount of extra mem that can be added with virtio-men
+    hypervisor = "cloud-hypervisor";
+    vcpu = 2;
+  };
+
+  services.immich = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 2284;
+    mediaLocation = "/stuff/photos";
+    openFirewall = true;
+  };
+
+  users.users.sam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "test";
+  };
+
+
+  services.openssh.enable = true;
+
+  systemd.network.enable = true;
+
+  systemd.network.networks."20-lan" = {
+    matchConfig.Type = "ether";
+    networkConfig = {
+      DHCP = "yes";
+    };
+  };
+
+  system.stateVersion = "25.11";
+}

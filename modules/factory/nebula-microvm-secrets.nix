4e9a52e78d75978bbcbdb9e2f5d4c021cb05b699
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-28 18:06:43 +1000

    Update nebula-microvm-secrets.nix

--- modules/factory/nebula-microvm-secrets.nix
+++ modules/factory/nebula-microvm-secrets.nix
@@ -1,32 +1,34 @@
+# flake.factory.nebulaSecrets
 {
   flake.factory.nebulaSecrets =
     { vm }:
-    { config, lib, inputs, ... }:
-
     let
+      # Capture inputs at factory-evaluation time (this is safe)
       sopsFile = "${inputs.secrets}/secrets/nebula.yaml";
     in
+    { config, lib, ... }:
     {
+      # Host-only module produced by the factory
       systemd.tmpfiles.rules = [
         "d /var/lib/microvms/${vm}/nebula 0750 root root -"
       ];
 
       sops.secrets."nebula.${vm}.ca" = {
-        inherit sopsFile;
+        sopsFile = sopsFile;
         key = "nebula/ca";
         path = "/var/lib/microvms/${vm}/nebula/ca.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.crt" = {
-        inherit sopsFile;
+        sopsFile = sopsFile;
         key = "nebula/${vm}/crt";
         path = "/var/lib/microvms/${vm}/nebula/host.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.key" = {
-        inherit sopsFile;
+        sopsFile = sopsFile;
         key = "nebula/${vm}/key";
         path = "/var/lib/microvms/${vm}/nebula/host.key";
         mode = "0400";

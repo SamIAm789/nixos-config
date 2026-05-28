651d6d7af168c7bfdb2f61db380ebc4d6d632649
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-28 13:27:41 +1000

    Update lib.nix

--- modules/microvms/_flake-parts/modules/lib.nix
+++ modules/microvms/_flake-parts/modules/lib.nix
@@ -13,19 +13,20 @@
   config.flake.lib = {
 
     mkMicroVM = system: name: {
-  ${name} = inputs.nixpkgs.lib.nixosSystem {
-    inherit system;
+      ${name} = inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
 
-    modules = [
-      inputs.microvm.nixosModules.microvm
-      self.modules.nixos.base
-      self.modules.nixos.${name}
+        modules = [
+          inputs.microvm.nixosModules.microvm
+          self.modules.nixos.base
+          self.modules.nixos.${name}
 
-      {
-        nixpkgs.hostPlatform = lib.mkDefault system;
-        networking.hostName = lib.mkDefault name;
-      }
-    ];
+          {
+            nixpkgs.hostPlatform = lib.mkDefault system;
+            networking.hostName = lib.mkDefault name;
+          }
+        ];
+      };
+    };
   };
-};
 }

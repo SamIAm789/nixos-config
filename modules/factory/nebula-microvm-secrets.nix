5e1bb3d5ea73b5c0ecff8b2941e62177e581592f
sam <samblackabc@gmail.com>
2026-05-29 21:56:13 +1000

    nebula secrets microvm

--- modules/factory/nebula-microvm-secrets.nix
+++ modules/factory/nebula-microvm-secrets.nix
@@ -15,21 +15,21 @@
       sops.secrets."nebula.${vm}.ca" = {
         sopsFile = sopsFile;
         key = "nebula/ca";
-        path = "/perist/microvms/state/${vm}/nebula/ca.crt";
+        path = "/persist/microvms/state/${vm}/nebula/ca.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.crt" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/crt";
-        path = "/perist/microvms/state/${vm}/nebula/host.crt";
+        path = "/persist/microvms/state/${vm}/nebula/host.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.key" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/key";
-        path = "/perist/microvms/state/${vm}/nebula/host.key";
+        path = "/persist/microvms/state/${vm}/nebula/host.key";
         mode = "0400";
       };
     };

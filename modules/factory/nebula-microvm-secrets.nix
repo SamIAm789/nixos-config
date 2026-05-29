6ef318d227a729a7b243934e684d7cdd611bcfff
SamIAm <139657965+SamIAm789@users.noreply.github.com>
2026-05-30 06:36:07 +1000

    Update nebula-microvm-secrets.nix
    changed factory function to produce a file rather than a symlink

--- modules/factory/nebula-microvm-secrets.nix
+++ modules/factory/nebula-microvm-secrets.nix
@@ -10,29 +10,25 @@
     in
     { config, lib, ... }:
     {
-      # Host-only module produced by the factory
-      systemd.tmpfiles.rules = [
-        "d /persist/microvms/state/${vm}/nebula 0750 root root -"
-      ];
 
       sops.secrets."nebula.${vm}.ca" = {
         sopsFile = sopsFile;
         key = "nebula/ca";
-        path = "/persist/microvms/state/${vm}/nebula/ca.crt";
+        path = "/run/secrets/nebula/${vm}/ca.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.crt" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/crt";
-        path = "/persist/microvms/state/${vm}/nebula/host.crt";
+        path = "/run/secrets/nebula/${vm}/nebula/host.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.key" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/key";
-        path = "/persist/microvms/state/${vm}/nebula/host.key";
+        path = "run/secrets/nebula/${vm}/host.key";
         mode = "0400";
       };
     };

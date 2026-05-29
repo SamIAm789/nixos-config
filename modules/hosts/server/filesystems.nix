3e1af250dc290ea4819b318e252253b00c5f26f7
sam <samblackabc@gmail.com>
2026-05-29 20:18:40 +1000

    added stuff to filesystems

--- modules/hosts/server/filesystems.nix
+++ modules/hosts/server/filesystems.nix
@@ -21,5 +21,65 @@
       fsType = "zfs";
       neededForBoot = true;
     };
+
+    fileSystems."/stuff" = {
+      device = "stuff";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/haos" = {
+      device = "stuff/haos";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/home-vids" = {
+      device = "stuff/home-vids";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/immich" = {
+      device = "stuff/immich";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/immich-test" = {
+      device = "stuff/immich-test";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/incus-backups" = {
+      device = "stuff/incus-backups";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/ocis" = {
+      device = "stuff/ocis";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/paperless" = {
+      device = "stuff/paperless";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/photos" = {
+      device = "stuff/photos";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/pinchflat" = {
+      device = "stuff/pinchflat";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/rochelle" = {
+      device = "stuff/rochelle";
+      fsType = "zfs";
+    };
+
+    fileSystems."/stuff/videos" = {
+      device = "stuff/videos";
+      fsType = "zfs";
+    };
   };
 }

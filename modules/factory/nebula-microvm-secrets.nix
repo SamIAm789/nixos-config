9c61e4e2c3a6243e92cba90c229f5fb26007ed82
sam <samblackabc@gmail.com>
2026-05-29 21:37:32 +1000

    nebula secrets microvm

--- modules/factory/nebula-microvm-secrets.nix
+++ modules/factory/nebula-microvm-secrets.nix
@@ -11,28 +11,25 @@
     { config, lib, ... }:
     {
       # Host-only module produced by the factory
-      systemd.tmpfiles.rules = [
-        "d /var/lib/microvms/${vm}/nebula 0750 root root -"
-      ];
 
       sops.secrets."nebula.${vm}.ca" = {
         sopsFile = sopsFile;
         key = "nebula/ca";
-        path = "/var/lib/microvms/${vm}/nebula/ca.crt";
+        path = "/perist/microvms/state/${vm}/nebula/ca.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.crt" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/crt";
-        path = "/var/lib/microvms/${vm}/nebula/host.crt";
+        path = "/perist/microvms/state/${vm}/nebula/host.crt";
         mode = "0400";
       };
 
       sops.secrets."nebula.${vm}.key" = {
         sopsFile = sopsFile;
         key = "nebula/${vm}/key";
-        path = "/var/lib/microvms/${vm}/nebula/host.key";
+        path = "/perist/microvms/state/${vm}/nebula/host.key";
         mode = "0400";
       };
     };
--- modules/microvms/_flake-parts/modules/nebula.nix
+++ modules/microvms/_flake-parts/modules/nebula.nix
@@ -15,28 +15,23 @@
     ];
 
     services.nebula.networks.pertaka = {
-      ca   = lib.mkForce "/run/secrets/nebula/ca.crt";
-      cert = lib.mkForce "/run/secrets/nebula/host.crt";
-      key  = lib.mkForce "/run/secrets/nebula/host.key";
+      ca   = lib.mkForce "/etc/nebula/ca.crt";
+      cert = lib.mkForce "/etc/nebula/host.crt";
+      key  = lib.mkForce "/etc/nebula/host.key";
     };
 
     microvm.shares = [
       {
         proto = "virtiofs";
-        tag = "nebula-secrets";
-        source = "/var/lib/microvms/${config.networking.hostName}/nebula";
-        mountPoint = "/run/secrets/nebula";
-        readOnly = true;
+            tag = "nebula-secrets";
+            source = "/persist/microvms/state/${config.networking.hostName}/nebula";
+            mountPoint = "/etc/nebula";
+            readOnly = true;
       }
     ];
     systemd.tmpfiles.rules = [
       # Fix directory permissions
-      "z /run/secrets/nebula 0750 nebula-pertaka nebula-pertaka -"
-
-      # Fix file permissions
-      "z /run/secrets/nebula/ca.crt   0400 nebula-pertaka nebula-pertaka -"
-      "z /run/secrets/nebula/host.crt 0400 nebula-pertaka nebula-pertaka -"
-      "z /run/secrets/nebula/host.key 0400 nebula-pertaka nebula-pertaka -"
+      "d /etc/nebula 0750 nebula-pertaka nebula-pertaka -"
     ];
   };
 }
